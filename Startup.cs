233c1ea3f9791b3e154b79e7d18d499f6b3fb938
Eli Schwarz <eschwarz0@gmail.com>
2020-01-14 21:06:53 -0500

    finsihed html css and js basics module

--- Startup.cs
+++ Startup.cs
@@ -21,25 +21,9 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("Hello WOrld");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseNodeModules();
         }
     }
 }
---
+++ wwwroot/js/index.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("HI");
+
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+
+    button.on("click", function () {
+        console.log("Buying item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text());
+    });
+
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(500);
+    });
+
+
+});
